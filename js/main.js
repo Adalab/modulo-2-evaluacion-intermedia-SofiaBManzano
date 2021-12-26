f8dbe4de7d01397d3c1692b3c6f6c411d3181eda
SofiaBManzano <sofiabmanzano@gmail.com>
2021-12-26 14:11:50 +0100

    new functions

--- js/main.js
+++ js/main.js
@@ -9,94 +9,91 @@ const computer = document.querySelector(".js-computer");
 const reset = document.querySelector(".js-reset");
 let humanWin = 0;
 let computerWin = 0;
-let clickNumber = 0;
+let moves = 0;
 //funciones
-function getRandomNumber() {
-  return Math.ceil(Math.random() * 10);
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
 }
 
+//generar la jugada aleatoria: ordenador
+function generarJugadaAleatoria() {
+  let moveComputer = "";
+  let number = getRandomNumber(9);
+  if (number <= 3) {
+    moveComputer = "piedra";
+  } else if (number <= 6) {
+    moveComputer = "papel";
+  } else {
+    moveComputer = "tijera";
+  }
+  return moveComputer;
+}
+//obtener la jugada de la usuaria
+function obtenerJugadaUsuaria() {
+  return optionSelect.value;
+}
+//comprarar jugadas y ver quien gana
+function playGame() {
+  let moveComputer = generarJugadaAleatoria();
+  let movePlayer = obtenerJugadaUsuaria();
+  console.log(`jugada computadora ${moveComputer}`);
+  console.log(`jugada usuaria ${movePlayer}`);
+  moves++; //aumentar el numero de jugadas
+  console.log(moves);
+  if (moveComputer === movePlayer) {
+    winner.innerHTML = "Empate!";
+  } else if (movePlayer === "piedra") {
+    if (moveComputer === "papel") {
+      winner.innerHTML = "Perdiste :(";
+      computerWin++;
+    } else if (moveComputer === "tijera") {
+      winner.innerHTML = "Has ganado! :)";
+      humanWin++;
+    }
+  } else if (movePlayer === "tijera") {
+    if (moveComputer === "piedra") {
+      winner.innerHTML = "Perdiste :(";
+      computerWin++;
+    } else if (moveComputer === "papel") {
+      winner.innerHTML = "Has ganado! :)";
+      humanWin++;
+    }
+  } else if (movePlayer === "papel") {
+    if (moveComputer === "tijera") {
+      winner.innerHTML = "Perdiste :(";
+      computerWin++;
+    } else if (moveComputer === "piedra") {
+      winner.innerHTML = "Has ganado! :)";
+      humanWin++;
+    }
+  }
+  computer.innerHTML = "Ordenador: " + computerWin;
+  human.innerHTML = "Jugador:  " + humanWin;
+}
+//mostrar resultados: texto y puntuación
+function gameOver() {
+  if (moves === 10) {
+    if (humanWin > computerWin) {
+      winner.innerHTML = "Has ganado el juego";
+    } else if (humanWin < computerWin) {
+      winner.innerHTML = "Has perdido el juego";
+    } else {
+      winner.innerHTML = "Empate";
+    }
+    button.classList.add("hidden");
+    reset.classList.remove("hidden");
+  }
+}
 //handler
-function handlerPlay() {
-  const randomNumber = getRandomNumber();
-  console.log(randomNumber);
-  /*cuando se seleccione una opción, se guarda en el botón */
-  const selectValue = optionSelect.value;
-  console.log(selectValue);
-  clickNumber++; //aumentar el numero de jugadas
-  console.log(clickNumber);
-  btnReset();
-  if (clickNumber === 10) {
-    reset.classList.remove("collapse");
-  }
-  /*lanza numero random*/
-  /*si es menor que 3 el movimiento es piedra: pierde tijera gana papel*/
-  if (randomNumber < 3 && selectValue === "tijera") {
-    console.log("la máquina cogió piedra");
-    winner.innerHTML = "Has perdido :(";
-    computerWin += 1;
-    computer.innerHTML = `Computadora: ${computerWin}`;
-  } else if (randomNumber < 3 && selectValue === "papel") {
-    console.log("la máquina cogió piedra");
-    winner.innerHTML = "Punto para ti :)";
-    humanWin += 1;
-    human.innerHTML = `Jugadora: ${humanWin}`;
-  } else if (randomNumber < 3 && selectValue === "piedra") {
-    console.log("la máquina cogió piedra");
-    winner.innerHTML = "¡Empate!";
-    humanWin += 1;
-    human.innerHTML = `Jugadora: ${humanWin}`;
-    computerWin += 1;
-    computer.innerHTML = `Computadora: ${computerWin}`;
-  } /*si es mayor o igual que 6 el movimiento es papel. pierde piedra y gana tijera*/ else if (
-    randomNumber >= 6 &&
-    selectValue === "piedra"
-  ) {
-    console.log("la máquina cogió papel");
-    winner.innerHTML = "Has perdido! :(";
-    computerWin += 1;
-    computer.innerHTML = `Computadora: ${computerWin}`;
-  } else if (randomNumber >= 6 && selectValue === "tijera") {
-    console.log("la máquina cogió papel");
-    winner.innerHTML = "Muy bien! punto!";
-    humanWin += 1;
-    human.innerHTML = `Jugadora: ${humanWin}`;
-  } else if (randomNumber >= 6 && selectValue === "papel") {
-    console.log("la máquina cogió papel");
-    winner.innerHTML = "¡Empate!";
-    humanWin += 1;
-    human.innerHTML = `Jugadora: ${humanWin}`;
-    computerWin += 1;
-    computer.innerHTML = `Computadora: ${computerWin}`;
-  } /*y sino el movimiento generado es tijera, pierde papel gana piedra*/ else if (
-    randomNumber > 3 &&
-    randomNumber < 6 &&
-    selectValue === "papel"
-  ) {
-    console.log("La máquina cogió tijera");
-    winner.innerHTML = "Has perdido :(";
-    computerWin += 1;
-    computer.innerHTML = `Computadora: ${computerWin}`;
-  } else if (randomNumber > 3 && randomNumber < 6 && selectValue === "tijera") {
-    console.log("la máquina cogió tijera");
-    winner.innerHTML = "¡Empate!";
-    humanWin += 1;
-    human.innerHTML = `Jugadora: ${humanWin}`;
-    computerWin += 1;
-    computer.innerHTML = `Computadora: ${computerWin}`;
-  } else if (randomNumber > 3 && randomNumber < 6 && selectValue === "piedra") {
-    console.log("la máquina cogió tijera");
-    winner.innerHTML = "¡Ganaste!";
-    humanWin += 1;
-    human.innerHTML = `Jugadora: ${humanWin}`;
-  } else if (selectValue === "selectPreview") {
-    winner.innerHTML = "Tienes que escoger una opción";
-  }
+function handlerPlay(event) {
+  event.preventDefault();
+  playGame();
+  gameOver();
 }
 
 function btnReset() {
   //   reset.classList.add("collapse");
-
-  if (clickNumber === 10) {
+  if (moves === 10) {
     if (humanWin > computerWin) {
       winner.innerHTML = "¡Has ganado a la máquina! :)";
     } else if (computerWin > humanWin) {
@@ -109,18 +106,18 @@ function btnReset() {
   }
 }
 function handleClickReload(event) {
+  event.preventDefault();
   button.classList.remove("collapse");
   reset.classList.add("collapse");
   event.preventDefault();
   humanWin = 0;
   computerWin = 0;
-  clickNumber = 0;
+  moves = 0;
   winner.innerHTML = "Vamos a jugar!";
-  computer.innerHTML = `Computadora: ${computerWin}`;
-  human.innerHTML = `Jugadora: ${humanWin}`;
+  computer.innerHTML = "Computadora:" + computerWin;
+  human.innerHTML = "Jugadora:" + humanWin;
 }
+
 //listeners
 button.addEventListener("click", handlerPlay);
-
-//buttonreset
 reset.addEventListener("click", handleClickReload);
